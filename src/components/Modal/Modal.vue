c33dc9961dad693052fd0b1e78fc945b8bffc13b
daidr <daidr@daidr.me>
2022-10-20 02:57:05 +0800

    feat: update Modal style

--- src/components/Modal/Modal.vue
+++ src/components/Modal/Modal.vue
@@ -1,265 +1,282 @@
 <script setup>
-import { ref, watch, onMounted, onUnmounted } from 'vue';
-import LoadingIcon from '../LoadingIcon.vue';
+  import { ref, watch, onMounted, onUnmounted } from "vue";
+  import LoadingIcon from "../LoadingIcon.vue";
+  import MCFrame from "../MCFrame.vue";
+  import MCButton from "../MCButton.vue";
 
-const props = defineProps({
-  /**
-   * 是否可见
-   */
-  visible: {
-    type: Boolean,
-    default: false,
-  },
-  visibleModifiers: {
-    type: Object,
-    default: () => ({}),
-  },
-  /**
-   * 确认按钮文本
-   */
-  okText: {
-    type: String,
-    default: '确认',
-  },
-  /**
-   * 取消按钮文本
-   */
-  cancelText: {
-    type: String,
-    default: '取消',
-  },
-  /**
-   * 按下按钮后是否关闭对话框
-   */
-  autoClose: {
-    type: Boolean,
-    default: true,
-  },
-  /**
-   * 对话框内容
-   */
-  content: {
-    type: String,
-    default: '',
-  },
-  /**
-   * 对话框内容是否居中
-   */
-  centerContent: {
-    type: Boolean,
-    default: false,
-  },
-  /**
-   * 对话框标题
-   */
-  title: {
-    type: String,
-    default: '提示',
-  },
-  /**
-   * 是否隐藏取消按钮
-   */
-  hideCancel: {
-    type: Boolean,
-    default: false,
-  },
-  /**
-   * 是否隐藏确认按钮
-   */
-  hideOk: {
-    type: Boolean,
-    default: false,
-  },
-  /**
-   * 是否显示遮罩
-   */
-  mask: {
-    type: Boolean,
-    default: true,
-  },
-  /**
-   * 是否渲染到body元素
-   */
-  renderToBody: {
-    type: Boolean,
-    default: true,
-  },
-  /**
-   * 是否处于加载状态
-   */
-  isLoading: {
-    type: [Boolean, Object],
-    default: false,
-  },
-  dynamic: {
-    type: Boolean,
-    default: false,
-  },
-  moveable: {
-    type: Boolean,
-    default: false,
-  },
-});
-
-const _isLoading = ref(props.isLoading);
-
-watch(
-  () => props.isLoading,
-  newVal => {
-    _isLoading.value = newVal;
-  },
-);
-
-const modalEl = ref();
-
-const emit = defineEmits(['cancel', 'ok', 'open', 'close', 'update:visible']);
-
-const _visible = !props.visibleModifiers['model']
-  ? ref(props.visible)
-  : computed({
-    get() {
-      return props.visible;
+  const props = defineProps({
+    /**
+     * 是否可见
+     */
+    visible: {
+      type: Boolean,
+      default: false,
     },
-    set(value) {
-      emit('update:visible', value);
+    visibleModifiers: {
+      type: Object,
+      default: () => ({}),
+    },
+    /**
+     * 确认按钮文本
+     */
+    okText: {
+      type: String,
+      default: "确认",
+    },
+    /**
+     * 取消按钮文本
+     */
+    cancelText: {
+      type: String,
+      default: "取消",
+    },
+    /**
+     * 按下按钮后是否关闭对话框
+     */
+    autoClose: {
+      type: Boolean,
+      default: true,
+    },
+    /**
+     * 对话框内容
+     */
+    content: {
+      type: String,
+      default: "",
+    },
+    /**
+     * 对话框内容是否居中
+     */
+    centerContent: {
+      type: Boolean,
+      default: false,
+    },
+    /**
+     * 对话框标题
+     */
+    title: {
+      type: String,
+      default: "提示",
+    },
+    /**
+     * 是否隐藏取消按钮
+     */
+    hideCancel: {
+      type: Boolean,
+      default: false,
+    },
+    /**
+     * 是否隐藏确认按钮
+     */
+    hideOk: {
+      type: Boolean,
+      default: false,
+    },
+    /**
+     * 是否显示遮罩
+     */
+    mask: {
+      type: Boolean,
+      default: true,
+    },
+    /**
+     * 是否渲染到body元素
+     */
+    renderToBody: {
+      type: Boolean,
+      default: true,
+    },
+    /**
+     * 是否处于加载状态
+     */
+    isLoading: {
+      type: [Boolean, Object],
+      default: false,
+    },
+    dynamic: {
+      type: Boolean,
+      default: false,
+    },
+    moveable: {
+      type: Boolean,
+      default: false,
     },
   });
 
-const close = () => {
-  _visible.value = false;
-  if (props.dynamic) {
-    unmountedHandler();
-    modalEl.value.parentNode &&
-      modalEl.value.parentNode.parentNode &&
-      modalEl.value.parentNode.parentNode.removeChild(modalEl.value.parentNode);
-  }
-};
+  const _isLoading = ref(props.isLoading);
 
-const open = () => {
-  _visible.value = true;
-};
+  watch(
+    () => props.isLoading,
+    (newVal) => {
+      _isLoading.value = newVal;
+    }
+  );
 
-const onCancelButtonClick = () => {
-  if (!_visible.value) return;
-  if (_isLoading.value) return;
-  emit('cancel');
-  if (props.autoClose && !_isLoading.value) {
-    close();
-  }
-};
+  const modalEl = ref();
 
-const onOkButtonClick = () => {
-  if (!_visible.value) return;
-  emit('ok');
-  if (props.autoClose) {
-    close();
-  }
-};
+  const emit = defineEmits(["cancel", "ok", "open", "close", "update:visible"]);
 
-const handleOpen = () => {
-  if (_visible.value) {
-    emit('open');
-  }
-};
+  const _visible = !props.visibleModifiers["model"]
+    ? ref(props.visible)
+    : computed({
+        get() {
+          return props.visible;
+        },
+        set(value) {
+          emit("update:visible", value);
+        },
+      });
 
-const handleClose = () => {
-  if (!_visible.value) {
-    emit('close');
-  }
-};
+  const close = () => {
+    _visible.value = false;
+    if (props.dynamic) {
+      unmountedHandler();
+      modalEl.value.parentNode &&
+        modalEl.value.parentNode.parentNode &&
+        modalEl.value.parentNode.parentNode.removeChild(
+          modalEl.value.parentNode
+        );
+    }
+  };
 
-const onEscKeyUp = e => {
-  if (e.keyCode === 27) {
-    onCancelButtonClick();
-  }
-};
+  const open = () => {
+    _visible.value = true;
+  };
 
-onMounted(() => {
-  document.addEventListener('keyup', onEscKeyUp);
-  document.addEventListener('pointermove', onMovePointerMove);
-  document.addEventListener('pointerup', onMovePointerUp);
-  addEventListener('resize', onWindowResize);
-});
+  const onCancelButtonClick = () => {
+    if (!_visible.value) return;
+    if (_isLoading.value) return;
+    emit("cancel");
+    if (props.autoClose && !_isLoading.value) {
+      close();
+    }
+  };
 
-const unmountedHandler = () => {
-  document.removeEventListener('keyup', onEscKeyUp);
-  document.removeEventListener('pointermove', onMovePointerMove);
-  document.removeEventListener('pointerup', onMovePointerUp);
-  removeEventListener('resize', onWindowResize);
-};
+  const onOkButtonClick = () => {
+    if (!_visible.value) return;
+    emit("ok");
+    if (props.autoClose) {
+      close();
+    }
+  };
 
-onUnmounted(unmountedHandler);
+  const handleOpen = () => {
+    if (_visible.value) {
+      emit("open");
+    }
+  };
 
-const setLoadingState = state => {
-  _isLoading.value = state;
-};
+  const handleClose = () => {
+    if (!_visible.value) {
+      emit("close");
+    }
+  };
 
-const modalContainerEl = ref();
-let pointerId = -1;
-const moveX = ref(0);
-const moveY = ref(0);
+  const onEscKeyUp = (e) => {
+    if (e.keyCode === 27) {
+      onCancelButtonClick();
+    }
+  };
 
-const onWindowResize = () => {
-  if (!props.moveable) return;
-  const screenW = (window.innerWidth - modalContainerEl.value.offsetWidth) / 2;
-  const screenH =
-    (window.innerHeight - modalContainerEl.value.offsetHeight) / 2;
+  onMounted(() => {
+    document.addEventListener("keyup", onEscKeyUp);
+    document.addEventListener("pointermove", onMovePointerMove);
+    document.addEventListener("pointerup", onMovePointerUp);
+    addEventListener("resize", onWindowResize);
+  });
 
-  if (moveX.value > screenW) {
-    moveX.value = screenW;
-  } else if (moveX.value < -screenW) {
-    moveX.value = -screenW;
-  }
-  if (moveY.value > screenH) {
-    moveY.value = screenH;
-  } else if (moveY.value < -screenH) {
-    moveY.value = -screenH;
-  }
-};
+  const unmountedHandler = () => {
+    document.removeEventListener("keyup", onEscKeyUp);
+    document.removeEventListener("pointermove", onMovePointerMove);
+    document.removeEventListener("pointerup", onMovePointerUp);
+    removeEventListener("resize", onWindowResize);
+  };
 
-const onMovePointerDown = e => {
-  if (!props.moveable) return;
-  if (pointerId !== -1) return;
-  pointerId = e.pointerId;
-};
+  onUnmounted(unmountedHandler);
 
-const onMovePointerMove = e => {
-  if (!props.moveable) return;
-  if (e.pointerId !== pointerId) return;
-  moveX.value += e.movementX;
-  moveY.value += e.movementY;
-  const screenW = (window.innerWidth - modalContainerEl.value.offsetWidth) / 2;
-  const screenH =
-    (window.innerHeight - modalContainerEl.value.offsetHeight) / 2;
+  const setLoadingState = (state) => {
+    _isLoading.value = state;
+  };
 
-  if (moveX.value > screenW) {
-    moveX.value = screenW;
-  } else if (moveX.value < -screenW) {
-    moveX.value = -screenW;
-  }
-  if (moveY.value > screenH) {
-    moveY.value = screenH;
-  } else if (moveY.value < -screenH) {
-    moveY.value = -screenH;
-  }
-};
+  const modalContainerEl = ref();
+  let pointerId = -1;
+  const moveX = ref(0);
+  const moveY = ref(0);
 
-const onMovePointerUp = e => {
-  if (!props.moveable) return;
-  if (pointerId !== e.pointerId) return;
-  pointerId = -1;
-};
+  const onWindowResize = () => {
+    if (!props.moveable) return;
+    const screenW =
+      (window.innerWidth - modalContainerEl.value.offsetWidth) / 2;
+    const screenH =
+      (window.innerHeight - modalContainerEl.value.offsetHeight) / 2;
 
-defineExpose({ close, open, setLoadingState });
+    if (moveX.value > screenW) {
+      moveX.value = screenW;
+    } else if (moveX.value < -screenW) {
+      moveX.value = -screenW;
+    }
+    if (moveY.value > screenH) {
+      moveY.value = screenH;
+    } else if (moveY.value < -screenH) {
+      moveY.value = -screenH;
+    }
+  };
+
+  const onMovePointerDown = (e) => {
+    if (!props.moveable) return;
+    if (pointerId !== -1) return;
+    pointerId = e.pointerId;
+  };
+
+  const onMovePointerMove = (e) => {
+    if (!props.moveable) return;
+    if (e.pointerId !== pointerId) return;
+    moveX.value += e.movementX;
+    moveY.value += e.movementY;
+    const screenW =
+      (window.innerWidth - modalContainerEl.value.offsetWidth) / 2;
+    const screenH =
+      (window.innerHeight - modalContainerEl.value.offsetHeight) / 2;
+
+    if (moveX.value > screenW) {
+      moveX.value = screenW;
+    } else if (moveX.value < -screenW) {
+      moveX.value = -screenW;
+    }
+    if (moveY.value > screenH) {
+      moveY.value = screenH;
+    } else if (moveY.value < -screenH) {
+      moveY.value = -screenH;
+    }
+  };
+
+  const onMovePointerUp = (e) => {
+    if (!props.moveable) return;
+    if (pointerId !== e.pointerId) return;
+    pointerId = -1;
+  };
+
+  defineExpose({ close, open, setLoadingState });
 </script>
 
 <template>
-  <teleport to="body" :disabled="!renderToBody">
-    <transition name="fade" @after-enter="handleOpen" @after-leave="handleClose">
+  <teleport to="#teleports" :disabled="!renderToBody">
+    <transition
+      name="fade"
+      @after-enter="handleOpen"
+      @after-leave="handleClose"
+    >
       <div v-if="_visible" ref="modalEl" class="modal">
         <div v-if="mask" class="mask" @click="onCancelButtonClick"></div>
-        <div class="transform-container" :style="{ transform: `translate(${moveX}px,${moveY}px)` }">
-          <div ref="modalContainerEl" class="modal-container" :class="{ moveable }">
+        <div
+          class="transform-container"
+          :style="{ transform: `translate(${moveX}px,${moveY}px)` }"
+        >
+          <MCFrame
+            ref="modalContainerEl"
+            class="modal-container"
+            :class="{ moveable }"
+          >
             <div class="title" @pointerdown="onMovePointerDown">
               <slot name="header">
                 <div class="title-default">{{ title }}</div>
@@ -269,15 +286,25 @@ defineExpose({ close, open, setLoadingState });
               <slot>{{ content }}</slot>
             </div>
             <div v-if="!hideCancel || !hideOk" class="buttons">
-              <div v-if="!hideCancel" class="cancel-btn" :class="{ hide: _isLoading }" @click="onCancelButtonClick">
+              <MCButton
+                v-if="!hideCancel"
+                class="cancel-btn"
+                :class="{ hide: _isLoading }"
+                @click="onCancelButtonClick"
+              >
                 {{ cancelText }}
-              </div>
-              <div v-if="!hideOk" class="ok-btn" :class="{ loading: _isLoading }" @click="onOkButtonClick">
+              </MCButton>
+              <MCButton
+                v-if="!hideOk"
+                class="ok-btn"
+                :class="{ loading: _isLoading }"
+                @click="onOkButtonClick"
+              >
                 <LoadingIcon v-if="_isLoading" />
-                {{ _isLoading ? '' : okText }}
-              </div>
+                {{ _isLoading ? "" : okText }}
+              </MCButton>
             </div>
-          </div>
+          </MCFrame>
         </div>
       </div>
     </transition>
@@ -285,94 +312,88 @@ defineExpose({ close, open, setLoadingState });
 </template>
 
 <style lang="scss" scoped>
-.modal {
-  @apply z-91 fixed;
+  .modal {
+    @apply z-91 fixed;
 
-  .mask {
-    @apply fixed top-0 left-0 right-0 bottom-0 bg-black/20;
-  }
-
-  .transform-container {
-    @apply fixed top-0 left-0 right-0 bottom-0 pointer-events-none;
-  }
-
-  .modal-container {
-    @apply max-h-85vh pointer-events-auto;
-    @apply flex flex-col;
-    @apply shadow-lg shadow-primary/10;
-    @apply fixed top-1/2 left-1/2 min-w-xs sm: min-w-sm md:max-w-3xl lg:max-w-4xl bg-primary-extralight rounded-xl p-5 transform-gpu -translate-x-1/2 -translate-y-1/2;
-
-    .title {
-      @apply pb-2 font-bold select-none;
-
-      .title-default {
-        @apply text-2xl text-primary text-center;
-      }
+    .mask {
+      @apply fixed top-0 left-0 right-0 bottom-0 bg-black/20;
     }
 
-    &.moveable {
+    .transform-container {
+      @apply fixed top-0 left-0 right-0 bottom-0 pointer-events-none;
+    }
+
+    .modal-container {
+      @apply max-h-85vh pointer-events-auto;
+      @apply fixed top-1/2 left-1/2 min-w-xs sm: min-w-sm md:max-w-3xl lg:max-w-4xl transform-gpu -translate-x-1/2 -translate-y-1/2;
+
+      &:deep(.mcframe__main) {
+        @apply p-5;
+        @apply flex flex-col;
+      }
+
       .title {
-        @apply cursor-move;
-      }
-    }
+        @apply pb-2 font-bold select-none;
 
-    .content {
-      @apply text-lg text-primary/80 whitespace-pre-wrap;
-      @apply overflow-auto;
-
-      &.center {
-        @apply text-center;
-      }
-    }
-
-    .buttons {
-      @apply flex justify-end space-x-4 mt-3;
-
-      .cancel-btn,
-      .ok-btn {
-        @apply py-1.5 px-5 rounded-lg cursor-pointer motion-safe: transition select-none transform-gpu h-9;
-
-        &:active {
-          @apply scale-90;
+        .title-default {
+          @apply text-2xl text-primary text-center;
         }
       }
 
-      .cancel-btn {
-        @apply bg-gray-400 bg-opacity-20 text-gray-600 motion-safe: transition opacity-100 pointer-events-auto;
-
-        &:hover {
-          @apply bg-opacity-30;
-        }
-
-        &.hide {
-          @apply opacity-0 pointer-events-none;
+      &.moveable {
+        .title {
+          @apply cursor-move;
         }
       }
 
-      .ok-btn {
-        @apply bg-primary text-white;
-        @apply flex items-center;
+      .content {
+        @apply text-lg text-primary/80 whitespace-pre-wrap;
+        @apply overflow-auto;
 
-        &:hover {
-          @apply bg-opacity-90;
+        &.center {
+          @apply text-center;
+        }
+      }
+
+      .buttons {
+        @apply flex justify-end space-x-4 mt-3;
+
+        .cancel-btn,
+        .ok-btn {
+          @apply cursor-pointer transition select-none transform-gpu;
+
+          &:deep(.mcbutton__main) {
+            @apply px-6 py-1;
+          }
         }
 
-        &.loading {
-          @apply opacity-50 pointer-events-none;
-          @apply max-w-15 text-2xl;
+        .cancel-btn {
+          @apply pointer-events-auto;
+
+          &.hide {
+            @apply opacity-0 pointer-events-none;
+          }
+        }
+
+        .ok-btn {
+          @apply flex items-center;
+
+          &.loading {
+            @apply opacity-50 pointer-events-none;
+            @apply text-2xl;
+          }
         }
       }
     }
   }
-}
 
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.15s ease;
-}
+  .fade-enter-active,
+  .fade-leave-active {
+    transition: opacity 0.15s ease;
+  }
 
-.fade-enter-from,
-.fade-leave-to {
-  opacity: 0;
-}
+  .fade-enter-from,
+  .fade-leave-to {
+    opacity: 0;
+  }
 </style>
